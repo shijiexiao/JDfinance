2ef166e5d04f1e200d381737dc45f6799c15b7c7
杰少帅 <jie305998@163.com>
2019-07-09 13:30:00 +0800

    add  ye wan

---
+++ app/js/home/borrow.vue
@@ -0,0 +1,43 @@
+<template>
+    <Panel :title="'E淘借贷'" :class="$style.panel">
+
+        <section :class="$style.content">
+            <router-link :to="{ name: 'home' }">
+                <img src="//img12.360buyimg.com/jrpmobile/jfs/t12721/42/2497849749/26333/ec584be4/5a421756N2416c88f.png?width=750&height=280" alt="">
+            </router-link>
+
+        </section>
+
+    </Panel>
+</template>
+
+<script>
+    import Panel from "../core/panel.vue"
+
+export default {
+        name: "borrow",
+    components:{
+      Panel
+    },
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style lang="scss" rel="stylesheet/scss" module>
+
+
+    @import "../../css/element";
+    .panel{
+        @include panel();
+        .content{
+            padding-bottom: 40px;
+            img{
+                display: block;
+                width: 100%;
+
+            }
+        }
+    }
+</style>
--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -2,19 +2,26 @@
     <div>
         <Heador></Heador>
         <Hslider></Hslider>
+        <novice></novice>
+        <borrow></borrow>
+        <money></money>
+        <product></product>
         <span>杰哥在线教你做人</span>
-
     </div>
 </template>
 
 <script>
     import Heador from '../public/header'
     import Hslider from './hslider'
+    import novice from './novice'
 
+    import borrow from './borrow'
+    import money from './money'
+    import product from './product'
 
     export default {
         components: {
-            Heador,Hslider
+            Heador,Hslider,novice,borrow,product,money,
         }
     }
 </script>
